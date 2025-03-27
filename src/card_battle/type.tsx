7cfbe7b5b592eaa00b72afb66b7a3ee0637848ac
MAIxxxIAM <1207108696@qq.com>
2025-03-27 22:17:50 +0800

    更新：扩展卡牌类型，添加护甲破坏类型，优化敌人 AI 策略，增加敌人类和战斗上下文接口

--- src/card_battle/type.tsx
+++ src/card_battle/type.tsx
@@ -1,15 +1,22 @@
 import { Random } from "koishi";
-import { PokemonPower } from "../battle";
+import { PokemonPower, Skill } from "../battle";
 import { PVP } from "../battle/pvp";
 import { Pokebattle } from "../model";
+import { getType } from "../utils/method";
+import { battleType } from "../utils/data";
 
 enum CardRarity {
-  Common, //5 0.625
-  Uncommon, //2 0.25
-  Rare, //1  0.125
+  Common,
+  Uncommon,
+  Rare,
+}
+enum WildPokemonType {
+  NormalWild,
+  UncommonPokemon,
+  Legendary,
 }
 
-type CardType = "attack" | "defense" | "skill" | "event";
+type CardType = "attack" | "defense" | "skill" | "event" | "armorBreak";
 
 type StatusType = "poison" | "strength" | "weak";
 
@@ -21,6 +28,8 @@ export interface CardCharacter {
   energy: number;
   maxEnergy: number;
   power: PokemonPower;
+  pokemonCategory: string[];
+  skill: Skill[];
 }
 
 interface StatusEffect {
@@ -35,7 +44,26 @@ export abstract class RougueCard {
     public readonly type: CardType,
     public readonly description: string,
     public readonly cost: number,
-    public readonly rarity: CardRarity
+    public readonly rarity: CardRarity,
+    public readonly cardCategory:
+      | "一般"
+      | "格斗"
+      | "飞行"
+      | "毒"
+      | "地面"
+      | "岩石"
+      | "虫"
+      | "幽灵"
+      | "钢"
+      | "火"
+      | "水"
+      | "草"
+      | "电"
+      | "超能力"
+      | "冰"
+      | "龙"
+      | "恶"
+      | "妖精" = "一般"
   ) {}
   abstract effect(user: CardCharacter, target?: CardCharacter): void;
 }
@@ -46,6 +74,7 @@ class CardPlayer implements CardCharacter {
   currentHp: number;
   armor = 0;
   energy: number;
+  currentHand: RougueCard[];
 
   constructor(
     players: Pokebattle,
@@ -56,7 +85,9 @@ class CardPlayer implements CardCharacter {
     ),
     public readonly power: PokemonPower = new PVP(players).power,
     public deck: RougueCard[] = CardPool.spawnCard(30),
-    public discardPile: RougueCard[] = []
+    public discardPile: RougueCard[] = [],
+    public skill: Skill[] = new PVP(players).skill,
+    public pokemonCategory: string[] = getType(new PVP(players).monster_1)
   ) {
     this.currentHp = maxHp;
     this.energy = maxEnergy;
@@ -154,5 +185,128 @@ class CardPool {
     pool: (new () => RougueCard)[]
   ): new () => RougueCard {
     return pool[Math.floor(Math.random() * pool.length)];
+  }
+}
+
+//战斗上下文
+interface CombatContext {
+  player: CardCharacter;
+  self: CardCharacter;
+  currentEnergy: number;
+  turnCount: number;
+}
+
+interface AIStrategy {
+  selectCard(hand: RougueCard[], context: CombatContext): RougueCard | null;
+}
+
+class EnemyAI implements AIStrategy {
+  private memory = {
+    playerArmorHistory: [] as number[],
+  };
+  selectCard(hand: RougueCard[], context: CombatContext): RougueCard | null {
+    this.memory.playerArmorHistory.push(context.player.armor);
+    const armorTrend = this.calculateArmorTrend();
+    const validCards = hand.filter((c) => c.cost <= context.currentEnergy);
+    const attackCard = validCards.filter((c) => c.type === "attack");
+    const armorBreakCard = validCards.filter((c) => c.type === "armorBreak");
+    const skillCard = validCards.filter(
+      (c) =>
+        c.type === "skill" &&
+        context.self.pokemonCategory.includes(c.cardCategory)
+    );
+    if (armorTrend > context.player.maxHp * 0.1) {
+      return armorBreakCard.reduce(
+        (maxCard, currCard) =>
+          currCard.cost > maxCard.cost ? currCard : maxCard,
+        hand[0]
+      );
+    }
+    const categoryEffect: number = this.calculateCategory(context);
+    if (categoryEffect > 1) {
+      return skillCard.reduce(
+        (maxCard, currCard) =>
+          currCard.cost > maxCard.cost ? currCard : maxCard,
+        hand[0]
+      );
+    }
+    if (attackCard.length != 0) {
+      return attackCard.reduce(
+        (maxCard, currCard) =>
+          currCard.cost > maxCard.cost ? currCard : maxCard,
+        hand[0]
+      );
+    }
+    return validCards.reduce(
+      (maxCard, currCard) =>
+        currCard.cost > maxCard.cost ? currCard : maxCard,
+      hand[0]
+    );
+  }
+  private calculateArmorTrend(): number {
+    // 计算护甲变化趋势
+    return this.memory.playerArmorHistory
+      .slice(-3)
+      .reduce((sum, curr, i, arr) => sum + (curr - (arr[i - 1] || 0)), 0);
+  }
+  private calculateCategory(context: CombatContext): number {
+    const self = context.self;
+    const player = context.player;
+    return (
+      battleType.data[self.pokemonCategory[0]][player.pokemonCategory[0]] *
+      battleType.data[self.pokemonCategory[0]][player.pokemonCategory[1]] *
+      battleType.data[self.pokemonCategory[1]][player.pokemonCategory[1]] *
+      battleType.data[self.pokemonCategory[1]][player.pokemonCategory[0]]
+    );
+  }
+}
+
+//敌人类
+class Enemy implements CardCharacter {
+  currentHp: number;
+  armor = 0;
+  energy: number;
+  enymyType: WildPokemonType;
+  aiStrategy: AIStrategy = new EnemyAI();
+  currentHand: RougueCard[];
+  constructor(
+    wildPokemon: Pokebattle,
+    public readonly name: string = new PVP(wildPokemon).name,
+    public readonly maxHp: number = new PVP(wildPokemon).maxHp,
+    public readonly maxEnergy: number = Math.floor(
+      new PVP(wildPokemon).power.speed / 45
+    ),
+    public readonly power: PokemonPower = new PVP(wildPokemon).power,
+    public deck: RougueCard[] = CardPool.spawnCard(30),
+    public discardPile: RougueCard[] = [],
+    public skill: Skill[] = new PVP(wildPokemon).skill,
+    public pokemonCategory: string[] = getType(new PVP(wildPokemon).monster_1)
+  ) {
+    const powerSum = wildPokemon.power.reduce(
+      (sum, curr) => sum + Number(curr),
+      0
+    );
+    if (powerSum > 1200) this.enymyType = WildPokemonType.Legendary;
+    else if (powerSum > 800) this.enymyType = WildPokemonType.UncommonPokemon;
+    else this.enymyType = WildPokemonType.NormalWild;
+    this.currentHp = maxHp;
+    this.energy = maxEnergy;
+  }
+  drawHand(size: number): RougueCard[] {
+    if (this.deck.length < size) {
+      this.deck = Random.shuffle(this.discardPile);
+    }
+    const hand = this.deck.splice(0, size);
+    this.discardPile.push(...hand);
+    return hand;
+  }
+  act(context: CombatContext) {
+    const selectedCard = this.aiStrategy.selectCard(this.currentHand, context);
+
+    if (selectedCard) {
+      context.currentEnergy -= selectedCard.cost;
+      selectedCard.effect(this, context.player);
+      this.currentHand = this.currentHand.filter((c) => c !== selectedCard);
+    }
   }
 }
