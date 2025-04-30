8715b0b2c65d40984593d3ff49c9a51db4390063
MAIxxxIAM <1207108696@qq.com>
2025-04-30 20:27:51 +0800

    更新：移除调试输出，简化初始化类型函数

--- src/card_battle/method.tsx
+++ src/card_battle/method.tsx
@@ -6,9 +6,7 @@ export function initType<O extends new (...args: any[]) => any>(
   type: O,
   args?: any
 ) {
-  console.dir(data.statusEffects, { depth: null });
   const restorData = Object.assign(new type(args), data);
   restorData.restor();
-  console.dir(restorData.statusEffects, { depth: null });
   return restorData;
 }
